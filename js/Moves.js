6cbfdb2520f9b56425859732490192a6c1abc939
Albin Ljunghusen <albinljunghusen@gmail.com>
2019-03-13 11:10:10 +0100

    more filename fixes

--- js/Moves.js
+++ js/Moves.js
@@ -1,55 +1,69 @@
 "use strict";
 function AttemptMove(_coord) {
-    if (activePiece.blocks.every(aliveBlock => IsValidCoord(Vector2D.add(aliveBlock.coord, _coord)))) {
-        activePiece.Move(_coord);
-        DrawPage();
-    }
+  if (
+    activePiece.blocks.every(aliveBlock =>
+      IsValidCoord(Vector2D.add(aliveBlock.coord, _coord))
+    )
+  ) {
+    activePiece.Move(_coord);
+    DrawPage();
+  }
 }
 function HardDrop() {
-    while (activePiece.blocks.every(aliveBlock => aliveBlock.coord.y < height - 1 &&
-        !CoordinateHasDead(new Vector2D(aliveBlock.coord.y + 1, aliveBlock.coord.x)))) {
-        activePiece.Move(new Vector2D(1, 0));
-    }
-    KillActivePiece();
-    ClearFullLines();
-    ResetTimer();
-    ActivateNextPiece();
-    DrawPage();
+  while (
+    activePiece.blocks.every(
+      aliveBlock =>
+        aliveBlock.coord.y < height - 1 &&
+        !CoordinateHasDead(
+          new Vector2D(aliveBlock.coord.y + 1, aliveBlock.coord.x)
+        )
+    )
+  ) {
+    activePiece.Move(new Vector2D(1, 0));
+  }
+  KillActivePiece();
+  ClearFullLines();
+  ResetTimer();
+  ActivateNextPiece();
+  DrawPage();
 }
 function AttemptRotate(wise) {
-    //This algorithm implements "Wall Kicks"
-    //http://tetris.wikia.com/wiki/SRS
-    let worldCoords = activePiece.GetWorldRotationCoords(wise);
-    let wallKickMoves = activePiece.type == Shape.I
-        ? [
-            [[0, 0], [0, -2], [0, 1], [-1, -2], [2, 1]],
-            [[0, 0], [0, -1], [0, 2], [2, -1], [-1, 2]],
-            [[0, 0], [0, 2], [0, -1], [1, 2], [-2, -1]],
-            [[0, 0], [0, 1], [0, -2], [-2, 1], [1, -2]]
+  //This algorithm implements "Wall Kicks"
+  //http://tetris.wikia.com/wiki/SRS
+  let worldCoords = activePiece.GetWorldRotationCoords(wise);
+  let wallKickMoves =
+    activePiece.type == Shape.I
+      ? [
+          [[0, 0], [0, -2], [0, 1], [-1, -2], [2, 1]],
+          [[0, 0], [0, -1], [0, 2], [2, -1], [-1, 2]],
+          [[0, 0], [0, 2], [0, -1], [1, 2], [-2, -1]],
+          [[0, 0], [0, 1], [0, -2], [-2, 1], [1, -2]]
         ]
-        : [
-            [[0, 0], [0, -1], [1, -1], [-2, 0], [-2, -1]],
-            [[0, 0], [0, 1], [-1, 1], [2, 0], [2, 1]],
-            [[0, 0], [0, 1], [1, 1], [-2, 0], [-2, 1]],
-            [[0, 0], [0, -1], [-1, -1], [2, 0], [2, -1]]
+      : [
+          [[0, 0], [0, -1], [1, -1], [-2, 0], [-2, -1]],
+          [[0, 0], [0, 1], [-1, 1], [2, 0], [2, 1]],
+          [[0, 0], [0, 1], [1, 1], [-2, 0], [-2, 1]],
+          [[0, 0], [0, -1], [-1, -1], [2, 0], [2, -1]]
         ];
-    for (let i = 0; i < 5; i++) {
-        let firstindex = activePiece.rotatestate;
-        if (wise == Rotation.Counterclockwise) {
-            firstindex = (firstindex + 3) % 4;
-        }
-        let addCoord = wallKickMoves[firstindex][i];
-        let fullCoord = new Vector2D(addCoord[0], addCoord[1]);
-        if (wise == Rotation.Counterclockwise) {
-            fullCoord = Vector2D.opposite(fullCoord);
-        }
-        if (worldCoords.every(coord => IsValidCoord(Vector2D.add(coord, fullCoord)))) {
-            activePiece.ChangeCoords(worldCoords);
-            activePiece.Move(fullCoord);
-            let increment = wise == Rotation.Clockwise ? 1 : -1;
-            activePiece.rotatestate += increment + 4;
-            activePiece.rotatestate %= 4;
-            return;
-        }
+  for (let i = 0; i < 5; i++) {
+    let firstindex = activePiece.rotatestate;
+    if (wise == Rotation.Counterclockwise) {
+      firstindex = (firstindex + 3) % 4;
     }
+    let addCoord = wallKickMoves[firstindex][i];
+    let fullCoord = new Vector2D(addCoord[0], addCoord[1]);
+    if (wise == Rotation.Counterclockwise) {
+      fullCoord = Vector2D.opposite(fullCoord);
+    }
+    if (
+      worldCoords.every(coord => IsValidCoord(Vector2D.add(coord, fullCoord)))
+    ) {
+      activePiece.ChangeCoords(worldCoords);
+      activePiece.Move(fullCoord);
+      let increment = wise == Rotation.Clockwise ? 1 : -1;
+      activePiece.rotatestate += increment + 4;
+      activePiece.rotatestate %= 4;
+      return;
+    }
+  }
 }
